896ae969a75dd944399ba3384b9eac997811080b
Falmban Ammar <devAspirant@gmail.com>
2015-11-08 16:40:43 +0300

    Create Array.swift

---
+++ Array.swift
@@ -0,0 +1,17 @@
+
+var arr=[2,4,6,8];
+arr.removeAtIndex(0);
+arr.append(10);
+
+
+// insert the value 
+var aboutMe=["name" : "Ammar", "age":29];
+var name = aboutMe["name"]!
+var age  = aboutMe["age"]!
+print("---------------------");
+// output 
+print("Array value : " , arr);
+print("Abotu me:", aboutMe);
+print("my Name:", name);
+print("my age:", age);
+print("---------------------");
