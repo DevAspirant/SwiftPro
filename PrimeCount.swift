11e03302d7276c03fbddec76e30d423ffb8c23c7
Falmban Ammar <devAspirant@gmail.com>
2015-11-09 16:08:15 +0300

    Create PrimeCount.swift

---
+++ PrimeCount.swift
@@ -0,0 +1,9 @@
+/*------/ prime count   /------*/
+var number = 2
+var isPrime = true 
+for(var i=2;i<=number;i++){
+    if number % i == 0{
+        isPrime=false;
+    }
+}
+print("is it Prime: " , isPrime);
