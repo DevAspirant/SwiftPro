c392dcbec0a1ff14325b869fca5c1cd91439123d
Falmban Ammar <devAspirant@gmail.com>
2015-11-09 16:07:32 +0300

    Create ArrayCount.swift

---
+++ ArrayCount.swift
@@ -0,0 +1,8 @@
+/*------/ array count   /------*/
+var arryAcount = [8, 4, 7, 2, 9, 12];
+var index2 = 0;
+print("---/array account/---")
+while(index2 < arryAcount.count){
+    arryAcount[index2] = arryAcount[index2] - 1;
+    index2++;
+}
