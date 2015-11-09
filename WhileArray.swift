a7c13b9519ff3e2dc696ada05650abe138cfff5c
Falmban Ammar <devAspirant@gmail.com>
2015-11-09 16:06:08 +0300

    Create WhileArray.swift

---
+++ WhileArray.swift
@@ -0,0 +1,8 @@
+/*------/ while array  /------*/
+var arry = [6, 2, 9, 1];
+var index1 = 0;
+print("-----/ while Array /-----");
+while(index1 < 4){
+    print("while array : ",arry[index1]);
+    index1++;
+}
