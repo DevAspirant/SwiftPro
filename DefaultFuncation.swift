8e2a5d72eeb4f67a48494c691eb8fa09779a79df
Falmban Ammar <devAspirant@gmail.com>
2015-12-07 16:45:45 +0300

    Create DefaultFuncation.swift

---
+++ DefaultFuncation.swift
@@ -0,0 +1,12 @@
+            /* Funcation 1 : without parameter  */ 
+func Func1(num : Int) -> Int 
+{
+    let Result = num * num ; 
+    return Result;
+}
+
+var x = 5 
+
+var re = Func1(x)
+println("Funcation : ")
+println(re)
